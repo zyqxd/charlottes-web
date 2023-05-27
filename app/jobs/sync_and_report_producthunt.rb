3a0a1a373eed9508d0ebad861b9819b9344b837e
David Zhang <zyq.david@gmail.com>
2023-05-27 16:54:48 -0400

    Working sync prs

--- app/jobs/sync_and_report_producthunt.rb
+++ app/jobs/sync_and_report_producthunt.rb
@@ -3,7 +3,37 @@
 class SyncAndReportProducthunt < ApplicationJob
   queue_as :cron
 
+  REPO = 'producthunt/producthunt'
+
+  # TODO(DZ): Github API can be moved into external
   def perform(*args)
-    puts "SyncAndReportProducthunt"
+    page = repo.rels[:pulls].get(query: { direction: 'asc', state: 'all' })
+    process_page(page)
+
+    while page.rels[:next]
+      page = page.rels[:next].get
+      process_page(page)
+    end
+  end
+
+  def process_page(pulls)
+    pulls.data.each do |pull_raw|
+      number = pull_raw.number
+      pull = repo.rels[:pulls].get(uri: { number: number })
+      raise "Pull ##{number} is not found" unless pull.present?
+
+      GithubPullRequest.update_or_create_with(pull)
+      Rails.logger.info "Processed pull ##{number}"
+    rescue StandardError => e
+      Rails.logger.error "Error processing pull ##{number}: #{e.message}"
+    end
+  end
+
+  def client
+    @client ||= Octokit::Client.new(access_token: Credentials.github_pat_token)
+  end
+
+  def repo
+    @repo ||= client.repo REPO
   end
 end
--- app/models/github_pull_request.rb
+++ app/models/github_pull_request.rb
@@ -29,4 +29,32 @@ class GithubPullRequest < ApplicationRecord
     closed: 'closed',
     merged: 'merged',
   }
+
+  class << self
+    def update_or_create_with(pull)
+      data = pull.data
+      record = GithubPullRequest.find_or_initialize_by(number: data.number)
+
+      record.update!(
+        title: data.title,
+        url: data.html_url,
+        state: data.state,
+        lines_added: data.additions,
+        lines_removed: data.deletions,
+        comments_count: data.comments,
+        commits_count: data.commits,
+        review_comments_count: data.review_comments,
+        author: data.user.login,
+        author_id: data.user.id,
+        author_url: data.user.html_url,
+        repository: data.base.repo.full_name,
+        repository_id: data.base.repo.id,
+        repository_url: data.base.repo.html_url,
+        created_at: data.created_at,
+        updated_at: data.updated_at,
+        merged_at: data.merged_at,
+        closed_at: data.closed_at,
+      )
+    end
+  end
 end
